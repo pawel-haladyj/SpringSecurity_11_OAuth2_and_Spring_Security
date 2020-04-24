ddf486b9d944078d2f03df2231286e605953535a
Pawel Haladyj <pawel@haladyj.pl>
2020-04-24 20:51:58 +0200

    initial commit

---
+++ src/main/java/pl/haladyj/springsecurity11/SpringSecurity11Application.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity11Application.class, args);
+	}
+
+}
