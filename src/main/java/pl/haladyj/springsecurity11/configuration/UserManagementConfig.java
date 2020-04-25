24f627137798a8b94020450708a474ad92ffe3f1
Pawel Haladyj <pawel@haladyj.pl>
2020-04-25 13:27:48 +0200

    password - authorization server

---
+++ src/main/java/pl/haladyj/springsecurity11/configuration/UserManagementConfig.java
@@ -0,0 +1,43 @@
+package pl.haladyj.springsecurity11.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class UserManagementConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetailsManager uds = new InMemoryUserDetailsManager();
+
+
+        UserDetails u = User
+                .withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        uds.createUser(u);
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
